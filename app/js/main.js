be9d2a0124f5fcc49b03e7d80b895b1216f052ba
BekzodDeveloper <bkzd18kholdarov@gmail.com>
2021-04-13 12:36:16 +0500

    turn on slick carousel

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
 $('.rightside-menu__btn').on('click', function () {
     console.log('Hello!');
 })
+
+// function initSlider(){
+//     $('.main-content__slides').slick({
+//         dots: false,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         autoplay: true
+//     })
+
+// };
+$(document).ready(function(){
+    $('.main-content__slides').slick({
+        dots: false,
+        infinite: true,
+        speed: 2000,
+        slidesToShow: 1,
+        autoplay: true
+    });
+});
+
